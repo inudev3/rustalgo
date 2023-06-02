30008d9ecaca300dbed0d3671e8e2e0be39b7e8a
Inu Jung <inust33@gmail.com>
2023-06-02 18:22:07 +0900

    Time: 65 ms (11.78%), Space: 4.8 MB (15.24%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.rs
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.rs
@@ -1,10 +1,11 @@
 impl Solution {
-    pub fn successful_pairs(spells: Vec<i32>, potions: Vec<i32>, success: i64) -> Vec<i32> {
+    pub fn successful_pairs(spells: Vec<i32>, mut potions: Vec<i32>, success: i64) -> Vec<i32> {
         let n =spells.len();
         let m = potions.len();
         let mut res = vec![0;n];
-        let mut sorted : Vec<i64> = potions.iter().map(|&x| x as i64).collect();
-        sorted.sort();
+        potions.sort();
+        let sorted:Vec<_> = potions.iter().map(|&x| x as i64).collect();
+        
         for i in 0..n{
             let bound = (success+(spells[i]-1) as i64)/(spells[i] as i64);
             let idx = sorted.partition_point(|&x|
