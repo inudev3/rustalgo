07f260a87da4f90c8ee6322be97f9715ec921b45
Inu Jung <inust33@gmail.com>
2023-06-02 18:20:29 +0900

    Time: 33 ms (60%), Space: 2.1 MB (66.67%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.rs
+++ 0611-valid-triangle-number/0611-valid-triangle-number.rs
@@ -1,17 +1,17 @@
 impl Solution {
-    pub fn triangle_number(nums: Vec<i32>) -> i32 {
+    pub fn triangle_number(mut nums: Vec<i32>) -> i32 {
         let mut cnt=0;
-        let mut sorted = nums.clone();
-        sorted.sort();
+        
+        nums.sort();
         let n = nums.len();
         if n<3{
             return 0
         }
         for i in 0..n-2{
-            if sorted[i]==0{continue;}
+            if nums[i]==0{continue;}
             let mut k=i+2;
             for j in i+1..n-1 {
-                while k<n && sorted[i]+sorted[j]>sorted[k]{k+=1;}
+                while k<n && nums[i]+nums[j]>nums[k]{k+=1;}
                 cnt+=k-j-1;
             }
         }
