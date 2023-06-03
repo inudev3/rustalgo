18a89553118d93d002785dd9c34d0259a746be39
Inu Jung <inust33@gmail.com>
2023-06-03 14:18:15 +0900

    Time: 11 ms (71.05%), Space: 2.3 MB (71.05%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.rs
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.rs
@@ -3,19 +3,16 @@ impl Solution {
         let  mut lo=1;
         let  mut hi= *piles.iter().max().unwrap_or(&100_000_000);
         //속도가 올라가면 시간이 줄고
-        //속도가 느려지면 시간이 
-        let h = h as i64;
-        let mut res = hi;
-        while lo<=hi{
+        //속도가 느려지면 시간이 늠
+        while lo<hi{
             let mid = lo+(hi-lo)/2;
-            let total:i64 = piles.iter().map(|&pile|(pile+mid-1) as i64/mid as i64).sum();
+            let total:i32 = piles.iter().map(|&pile|(pile+mid-1)/mid).sum();
             if total<=h{
-                res= std::cmp::min(res,mid);
-                hi=mid-1;
+                hi=mid;
             }else{
                 lo=mid+1;
             }
         }
-        res
+        hi
     }
 }
