9483aec3ba9acb8d95e57538e6dfb981b9167ad5
Inu Jung <inust33@gmail.com>
2023-06-03 14:32:08 +0900

    Time: 15 ms (54.55%), Space: 2.4 MB (81.82%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.rs
@@ -0,0 +1,26 @@
+impl Solution {
+    pub fn ship_within_days(weights: Vec<i32>, days: i32) -> i32 {
+        let mut lo=*weights.iter().max().unwrap_or(&1);
+        let mut hi = weights.iter().sum();
+        while lo<hi{
+            let mid = lo+(hi-lo)/2;
+            let mut cnt=1;
+            let mut sum=0;
+            weights.iter().for_each(|&x|{
+                sum+=x;
+                if sum>mid{
+                    sum=x;
+                    cnt+=1;
+                }
+            });
+            //용량을 줄이면 날짜가 늘고
+            //용량을 늘리면 날짜가 줌
+            if cnt<=days{
+                hi=mid;
+            }else{
+                lo=mid+1;
+            }
+        }
+        return hi
+    }
+}
