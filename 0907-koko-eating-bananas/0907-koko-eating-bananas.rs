0118b8ca761444f649a8b8eefbeb35f997572a02
Inu Jung <inust33@gmail.com>
2023-06-03 14:14:24 +0900

    Time: 13 ms (36.84%), Space: 2.3 MB (32.89%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.rs
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.rs
@@ -1,18 +1,32 @@
+use std::cmp::min;
+use std::cmp::Ordering::*;
 impl Solution {
     pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
-        let  mut lo=1;
-        let  mut hi= *piles.iter().max().unwrap_or(&100_000_000);
-        //속도가 올라가면 시간이 줄고
-        //속도가 느려지면 시간이 늠
-        while lo<hi{
-            let mid = lo+(hi-lo)/2;
-            let total:i32 = piles.iter().map(|&pile|(pile+mid-1)/mid).sum();
-            if total<=h{
-                hi=mid;
-            }else{
-                lo=mid+1;
+        let h = h as i64;
+        let mut left = 1;
+        let mut right = piles.iter().max().unwrap().clone();
+        let mut res = right;
+        //속도가 낮으면 시간이 높고
+        //속도가 높으면 시간이 낮음
+        while left <= right {
+            let k = (right + left) / 2;
+            let mut hours: i64 = 0;
+            for x in piles.iter() {
+                hours += (x / k) as i64;
+                if x % k > 0 {
+                    hours += 1;
+                }
+            }
+            match hours.cmp(&h) {
+                Less | Equal => {
+                    res = min(res, k);
+                    right = k - 1;
+                }
+                Greater => {
+                    left = k + 1;
+                }
             }
         }
-        hi
+        res
     }
 }
