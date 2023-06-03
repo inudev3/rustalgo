1e96584e6316f38a97e49e9b37a84e42c030f6c1
Inu Jung <inust33@gmail.com>
2023-06-03 14:55:23 +0900

    Time: 9 ms (58.82%), Space: 2.5 MB (76.47%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.rs
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.rs
@@ -1,16 +1,19 @@
 impl Solution {
     pub fn smallest_divisor(nums: Vec<i32>, threshold: i32) -> i32 {
-        let mut lo=1 ;
-        let mut hi:i32 = *nums.iter().max().unwrap()+1;
-        while lo<hi{
-            let mid = lo+(hi-lo)/2;
-            let total:i64=nums.iter().map(|&x|(x+mid-1) as i64/mid as i64).sum();
-            if total<=threshold as i64{
-                hi=mid;
-            }else{
-                lo=mid+1;
+        let mut lo = 1;
+        let mut hi: i32 = *nums.iter().max().unwrap_or(&1000000);
+
+        while lo < hi {
+            let mid = lo + (hi - lo) / 2;
+
+            let total: i64 = nums.iter().fold(0, |acc, &x| acc + (x as i64 + mid as i64 - 1) / mid as i64);
+
+            if total <= threshold as i64 {
+                hi = mid;
+            } else {
+                lo = mid + 1;
             }
         } 
-        return hi as i32
+        return lo;
     }
 }
