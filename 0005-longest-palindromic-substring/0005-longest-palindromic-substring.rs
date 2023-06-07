2b183af63e93f982c1409ae34383ff507b8320b4
Inu Jung <inust33@gmail.com>
2023-06-07 17:12:43 +0900

    Time: 70 ms (36.64%), Space: 3.1 MB (9.91%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.rs
@@ -0,0 +1,30 @@
+impl Solution {
+    pub fn longest_palindrome(s: String) -> String {
+        let chars:Vec<_> = s.chars().collect();
+        let n= chars.len();
+        let mut dp = vec![vec![false;n];n];
+        for i in 0..n{
+            dp[i][i] = true;
+        }
+        let mut ans = vec![0,0];
+        for i in 0..n-1{
+            if chars[i]==chars[i+1]{
+                dp[i][i+1]=true;
+                ans[0] = i;
+                ans[1]=i+1;
+            }
+        }
+        for len in 2..n{
+            for i in 0..n-len{
+                let j = i+len;
+                if chars[i]==chars[j]&&dp[i+1][j-1]{
+                    dp[i][j]=true;
+                    ans[0]=i;
+                    ans[1]=j;
+                }
+            }
+        }
+        let res:String=chars[ans[0]..=ans[1]].iter().collect();
+        res
+    }
+}
