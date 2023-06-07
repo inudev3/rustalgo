d86c2eed3ceca093c0cb5e69152d0e60ae4cf77d
Inu Jung <inust33@gmail.com>
2023-06-07 12:55:42 +0900

    Time: 3 ms (67.24%), Space: 2.3 MB (84.48%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.rs
+++ 0261-graph-valid-tree/0261-graph-valid-tree.rs
@@ -1,27 +1,27 @@
 use std::collections::{HashSet,HashMap,VecDeque};
 impl Solution {
     pub fn valid_tree(n: i32, edges: Vec<Vec<i32>>) -> bool {
+        if edges.len()!=n as usize-1{
+            return false
+        }
         let mut graph = vec![vec![];n as usize];
         for edge in edges.iter(){
             graph[edge[0] as usize].push(edge[1]);
             graph[edge[1] as usize].push(edge[0]);
         }
-        let mut parent = HashMap::new();
-        parent.insert(0,-1);
+        let mut seen = HashSet::new();
+        seen.insert(0);
         let mut queue = VecDeque::new();
         queue.push_back(0);
         while let Some(node) = queue.pop_front(){
             for &next in graph[node as usize].iter(){
-                if *parent.get(&node).unwrap_or(&-1) == next{
+                if seen.contains(&next){
                     continue
                 }
-                if parent.contains_key(&next){
-                    return false
-                }
-                queue.push_back(next);
-                parent.insert(next,node);
+                seen.insert(next);
+                queue.push_back(next)
             }
         }
-        return parent.len()==n as usize
+        return seen.len()==n as usize
     }
 }
