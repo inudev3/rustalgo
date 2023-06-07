b91f4b4c39de2c26af528ece10609ffc1e5f4eec
Inu Jung <inust33@gmail.com>
2023-06-07 13:03:52 +0900

    Time: 2 ms (86.21%), Space: 2.3 MB (84.48%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.rs
+++ 0261-graph-valid-tree/0261-graph-valid-tree.rs
@@ -1,54 +1,44 @@
 use std::collections::{HashSet,HashMap,VecDeque};
-pub struct UnionFind {
-    root: Vec<usize>,
-    rank: Vec<usize>,
+
+
+pub fn union(root: &mut Vec<usize>, rank: &mut Vec<usize>, x: usize, y: usize)->bool {
+    let root_x = find(root, x);
+    let root_y = find(root, y);
+    if root_x != root_y {
+        if rank[root_x] > rank[root_y] {
+            root[root_y] = root_x;
+        } else if rank[root_x] < rank[root_y] {
+            root[root_x] = root_y;
+        } else {
+            root[root_y] = root_x;
+            rank[root_x] += 1;
+        }
+        return true
+    }
+    false
 }
 
-impl UnionFind {
-    pub fn new(size: usize) -> Self {
-        Self {
-            root: (0..size).collect(),
-            rank: vec![1; size],
-        }
-    }
-
-    pub fn union(&mut self, x: usize, y: usize)->bool {
-        let root_x = self.find(x);
-        let root_y = self.find(y);
-        if root_x != root_y {
-            if self.rank[root_x] > self.rank[root_y] {
-                self.root[root_y] = root_x;
-            } else if self.rank[root_x] < self.rank[root_y] {
-                self.root[root_x] = root_y;
-            } else {
-                self.root[root_y] = root_x;
-                self.rank[root_x] += 1;
-            }
-            return true
-        }
-        false
-    }
-
-    pub fn find(&mut self, x: usize) -> usize {
-        if x == self.root[x] {
-            x
-        } else {
-            self.root[x] = self.find(self.root[x]);
-            self.root[x]
-        }
+pub fn find(root: &mut Vec<usize>, x: usize) -> usize {
+    if x == root[x] {
+        x
+    } else {
+        root[x] = find(root, root[x]);
+        root[x]
     }
 }
 
 impl Solution {
     pub fn valid_tree(n: i32, edges: Vec<Vec<i32>>) -> bool {
-        if edges.len()!=n as usize -1{
+        let n = n as usize;
+        if edges.len()!=n -1{
             return false
         }
-        let mut uf = UnionFind::new(n as usize);
+        let mut root:Vec<usize> = (0..n).collect();
+        let mut rank = vec![1;n];
         for edge in edges.iter(){
             let x = edge[0] as usize;
             let y = edge[1] as usize;
-            if !uf.union(x,y){
+            if !union(&mut root, &mut rank,x,y){
                 return false
             }
         }
