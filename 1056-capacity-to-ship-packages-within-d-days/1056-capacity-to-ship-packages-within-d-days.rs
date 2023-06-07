1290b50b81728a1a2f8356c20af7bb0b9bd284c8
Inu Jung <inust33@gmail.com>
2023-06-07 16:28:26 +0900

    Time: 15 ms (61.54%), Space: 2.4 MB (69.23%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.rs
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.rs
@@ -1,25 +1,24 @@
 impl Solution {
     pub fn ship_within_days(weights: Vec<i32>, days: i32) -> i32 {
-        let mut lo=*weights.iter().max().unwrap_or(&1);
-        let mut hi = 25*1000000;
-        while lo<hi{
+        let mut lo= *weights.iter().max().unwrap_or(&1);
+        let mut hi= 25000000;
+        while lo<hi {
             let mid = lo+(hi-lo)/2;
-            
-            let (cnt, _)=weights.iter().fold((1,0),|(cnt,sum),&x|{
-               if sum+x>mid{
-                   (cnt+1,x)
-               }else{
-                   (cnt,sum+x)
-               }
-            });
-            //용량을 줄이면 날짜가 늘고
-            //용량을 늘리면 날짜가 줌
+            let mut sum = 0;
+            let mut cnt = 1;
+            for weight in &weights {
+                sum += *weight;
+                if sum > mid {
+                    sum = *weight;
+                    cnt += 1;
+                }
+            }
             if cnt<=days{
                 hi=mid;
             }else{
                 lo=mid+1;
             }
         }
-        return hi
+        hi
     }
 }
