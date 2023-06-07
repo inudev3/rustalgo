151d08fccd761f5656574cee73c9a5603951f4fe
Inu Jung <inust33@gmail.com>
2023-06-07 18:34:57 +0900

    Time: 25 ms (5%), Space: 2.5 MB (65%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.rs
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.rs
@@ -1,19 +1,25 @@
 use std::cmp::min;
 impl Solution {
     pub fn minimum_delete_sum(s1: String, s2: String) -> i32 {
-        let mut memo =vec![vec![-1;s2.len()];s1.len()];
-        solve(s1.as_bytes(), s2.as_bytes(),0,0,&mut memo)
+        let m = s1.len();
+        let n = s2.len();
+        let s1 = s1.as_bytes();
+        let s2 = s2.as_bytes();
+        let mut memo =vec![vec![0;n+1];m+1];
+        for i in (0..m).rev(){
+            memo[i][n] = memo[i+1][n]+s1[i] as i32;
+        }
+        for i in (0..n).rev(){
+            memo[m][i] = memo[m][i+1]+s2[i] as i32;;
+        }
+        solve(s1,s2,0,0,&mut memo)
     }
 }
 fn solve(s1:&[u8],s2:&[u8],i:usize,j:usize,memo:&mut Vec<Vec<i32>>)->i32{
-    //
-    if i==s1.len(){
-        return s2[j..].iter().map(|&x|x as i32).sum()
+    if i == s1.len() || j == s2.len() {
+        return memo[i][j]
     }
-    if j==s2.len(){
-        return s1[i..].iter().map(|&x|x as i32).sum()
-    }
-    if memo[i][j]!=-1{
+    if memo[i][j]!=0{
         return memo[i][j]
     }
     memo[i][j]= if s1[i]==s2[j]{
