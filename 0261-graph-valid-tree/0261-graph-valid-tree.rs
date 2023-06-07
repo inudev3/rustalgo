4b3befad04818ccecb2ec7ecd8b5d266154ca643
Inu Jung <inust33@gmail.com>
2023-06-07 13:00:22 +0900

    Time: 3 ms (67.24%), Space: 2.4 MB (84.48%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.rs
+++ 0261-graph-valid-tree/0261-graph-valid-tree.rs
@@ -1,27 +1,57 @@
 use std::collections::{HashSet,HashMap,VecDeque};
+pub struct UnionFind {
+    root: Vec<usize>,
+    rank: Vec<usize>,
+}
+
+impl UnionFind {
+    pub fn new(size: usize) -> Self {
+        Self {
+            root: (0..size).collect(),
+            rank: vec![1; size],
+        }
+    }
+
+    pub fn union(&mut self, x: usize, y: usize)->bool {
+        let root_x = self.find(x);
+        let root_y = self.find(y);
+        if root_x != root_y {
+            if self.rank[root_x] > self.rank[root_y] {
+                self.root[root_y] = root_x;
+            } else if self.rank[root_x] < self.rank[root_y] {
+                self.root[root_x] = root_y;
+            } else {
+                self.root[root_y] = root_x;
+                self.rank[root_x] += 1;
+            }
+            return true
+        }
+        false
+    }
+
+    pub fn find(&mut self, x: usize) -> usize {
+        if x == self.root[x] {
+            x
+        } else {
+            self.root[x] = self.find(self.root[x]);
+            self.root[x]
+        }
+    }
+}
+
 impl Solution {
     pub fn valid_tree(n: i32, edges: Vec<Vec<i32>>) -> bool {
-        if edges.len()!=n as usize-1{
+        if edges.len()!=n as usize -1{
             return false
         }
-        let mut graph = vec![vec![];n as usize];
+        let mut uf = UnionFind::new(n as usize);
         for edge in edges.iter(){
-            graph[edge[0] as usize].push(edge[1]);
-            graph[edge[1] as usize].push(edge[0]);
-        }
-        let mut seen = HashSet::new();
-        seen.insert(0);
-        let mut queue = VecDeque::new();
-        queue.push_back(0);
-        while let Some(node) = queue.pop_front(){
-            for &next in graph[node as usize].iter(){
-                if seen.contains(&next){
-                    continue
-                }
-                seen.insert(next);
-                queue.push_back(next)
+            let x = edge[0] as usize;
+            let y = edge[1] as usize;
+            if !uf.union(x,y){
+                return false
             }
         }
-        return seen.len()==n as usize
+        true
     }
 }
