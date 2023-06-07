8bb8f02c497b3730a8aeb33d85b6e92e8d400144
Inu Jung <inust33@gmail.com>
2023-06-07 16:18:07 +0900

    Time: 0 ms (100%), Space: 2.4 MB (48.94%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.rs
+++ 0740-delete-and-earn/0740-delete-and-earn.rs
@@ -9,23 +9,11 @@ impl Solution {
             *score.entry(num).or_insert(0) += num;
             maxnum = max(maxnum,num);
         }
-        let mut memo = vec![-1;(maxnum+1) as usize];
-        solve(&mut memo, &nums, &score, maxnum as usize)
+        let mut dp = vec![0;(maxnum+1) as usize];
+        dp[1] = *score.get(&1).unwrap_or(&0);
+        for i in 2..maxnum as usize +1{
+            dp[i] = max(dp[i-2]+ *score.get(&(i as i32)).unwrap_or(&0), dp[i-1]);
+        }
+        dp[maxnum as usize]
     }
-}
-fn solve(memo:&mut Vec<i32>, nums:&Vec<i32>,score:&HashMap<i32,i32>,num:usize)->i32{
-    if num==0{
-        return 0
-    }
-    if num==1{
-        return *score.get(&1).unwrap_or(&0)
-    }
-    if memo[num]!=-1{
-        return memo[num]
-    }
-    let mut ans=0;
-    let gain = score.get(&(num as i32)).unwrap_or(&0);
-    ans = max(*gain+solve(memo,nums,score,num-2), solve(memo,nums,score,num-1));
-    memo[num]=ans;
-    ans
 }
