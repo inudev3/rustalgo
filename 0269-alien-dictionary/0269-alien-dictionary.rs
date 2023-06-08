80104ded85d4dc7ac0afd2fe644c94972fa62cb1
Inu Jung <inust33@gmail.com>
2023-06-08 19:13:49 +0900

    Time: 1 ms (72%), Space: 2.1 MB (60%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.rs
+++ 0269-alien-dictionary/0269-alien-dictionary.rs
@@ -1,19 +1,14 @@
 use std::collections::{HashMap, HashSet, VecDeque};
 impl Solution{
     pub fn alien_order(words: Vec<String>) -> String {
-        let mut adj: HashMap<char, Vec<char>> = HashMap::new();
-        let mut indegree: HashMap<char, usize> = HashMap::new();
-        let mut set: HashSet<char> = HashSet::new();
-        let words: Vec<Vec<char>> = words.into_iter().map(|s| s.chars().collect()).collect();
+        let mut adj = vec![vec![];26];
+        let mut indegree = vec![0;26];
+        let mut set: HashSet<u8> = HashSet::new();
+        let words: Vec<Vec<u8>> = words.into_iter().map(|s| s.into_bytes()).collect();
         for word in &words {
-            for c in word {
-                set.insert(*c);
+            for &c in word {
+                set.insert(c-b'a');
             }
-        }
-
-        for &ch in &set {
-            adj.insert(ch, vec![]);
-            indegree.insert(ch, 0);
         }
 
         for i in 0..words.len()-1 {
@@ -25,39 +20,38 @@ impl Solution{
 
             for j in 0..std::cmp::min(word1.len(), word2.len()) {
                 if word1[j] != word2[j] {
-                    indegree.entry(word2[j]).and_modify(|e|  *e += 1 );
-                    adj.entry(word1[j]).and_modify(|e| e.push(word2[j]));
+                    let a =(word1[j]-b'a') as usize;
+                    let b = (word2[j]-b'a') as usize;
+                    indegree[b]+=1;
+                    adj[a].push(word2[j]);
                     break;
                 }
             }
         }
 
-        let mut queue: VecDeque<char> = VecDeque::new();
-        for (&k, &v) in &indegree {
-            if v == 0 {
-                queue.push_back(k);
+        let mut queue: VecDeque<u8> = VecDeque::new();
+        for i in 0..26 as u8{
+            if set.contains(&i)&&indegree[i as usize] == 0 {
+                queue.push_back(i as u8);
             }
         }
 
-        let mut result = String::new();
+        let mut result = vec![];
         while let Some(curr)= queue.pop_front() {
-            result.push(curr);
-            if let Some(next_chars) = adj.get(&curr) {
-                for &next in next_chars {
-                    if let Some(count) = indegree.get_mut(&next) {
-                        *count -= 1;
-                        if *count == 0 {
-                            queue.push_back(next);
-                        }
-                    }
-                }
+            result.push(curr+b'a');
+            for &next in &adj[curr as usize] {     
+                let a= (next-b'a') as usize;            
+               indegree[a] -= 1;
+                if indegree[a] == 0 {
+                    queue.push_back(a as u8);
+                }             
             }
         }
 
-        if result.len() < indegree.len() {
+        if result.len() < set.len() {
             String::new()
         } else {
-            result
+            String::from_utf8(result).unwrap()
         }
     }
 }
