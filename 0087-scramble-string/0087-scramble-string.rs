aeb499cda523e4c2dcc0ede5cd148918980c29d3
Inu Jung <inust33@gmail.com>
2023-06-16 14:12:05 +0900

    Time: 13 ms (13.89%), Space: 2.3 MB (41.67%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.rs
+++ 0087-scramble-string/0087-scramble-string.rs
@@ -3,36 +3,22 @@ impl Solution {
         let s1 = s1.as_bytes();
         let s2 = s2.as_bytes();
         let n = s1.len();
-        let mut dp = vec![vec![vec![Option::None;n];n];n+1];
-        helper(s1, s2, n, 0, 0, &mut dp)
+        let mut dp = vec![vec![vec![false;n];n];n+1];
+        for i in 0..n{
+            for j in 0..n{
+                dp[1][i][j] = s1[i]==s2[j];
+            }
+        }
+        for len in 2..=n{
+            for i in 0..=n-len{
+                for j in 0..=n-len{
+                    for newlen in 1..len{
+                        dp[len][i][j] |= dp[newlen][i][j] && dp[len-newlen][i+newlen][j+newlen];
+                        dp[len][i][j] |= dp[newlen][i][j+len-newlen] && dp[len-newlen][i+newlen][j];
+                    }
+                }
+            }
+        }
+        dp[n][0][0]
     }
 }
-fn helper(s1: &[u8], s2: &[u8], len: usize, i: usize, j: usize, dp: &mut Vec<Vec<Vec<Option<bool>>>>) -> bool {
-        if let Some(result) = dp[len][i][j] {
-            return result;
-        }
-        if &s1[i..i+len] == &s2[j..j+len] {
-            dp[len][i][j] = Some(true);
-            return true;
-        }
-        let mut cnt = vec![0; 26];
-        for k in 0..len {
-            cnt[(s1[i+k] - b'a') as usize] += 1;
-            cnt[(s2[j+k] - b'a') as usize] -= 1;
-        }
-        for ch in 0..26 {
-            if cnt[ch] != 0 {
-                dp[len][i][j] = Some(false);
-                return false;
-            }
-        }
-        for newlen in 1..len {
-            if (helper(s1, s2, newlen, i, j, dp) && helper(s1, s2, len - newlen, i + newlen, j + newlen, dp)) ||
-               (helper(s1, s2, newlen, i, j + len - newlen, dp) && helper(s1, s2, len - newlen, i + newlen, j, dp)) {
-                dp[len][i][j] = Some(true);
-                return true;
-            }
-        }
-        dp[len][i][j] = Some(false);
-        false
-    }
