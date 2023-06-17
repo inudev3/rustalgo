2fb1c32d5e8d35e8629e6602637aef7c5069d3ec
Inu Jung <inust33@gmail.com>
2023-06-17 16:01:46 +0900

    Time: 23 ms (61.64%), Space: 2.3 MB (64.84%) - LeetHub

--- 0911-profitable-schemes/0911-profitable-schemes.rs
+++ 0911-profitable-schemes/0911-profitable-schemes.rs
@@ -1,25 +1,17 @@
-const MOD:i32= 1_000_000_007;
 impl Solution {
-    pub fn profitable_schemes(n: i32, min_profit: i32, group: Vec<i32>, profit: Vec<i32>) -> i32 {
-        let sum:i32 = profit.iter().map(|x|*x).sum();
-        let mut dp = vec![vec![vec![-1;sum as usize+1];n as usize+1];group.len()];
-        solve(min_profit,&profit,&group,&mut dp, n as usize,0,0)
+    pub fn profitable_schemes(n: i32, mip: i32, g: Vec<i32>, p: Vec<i32>) -> i32 {
+        let (mut dp, mut mask, n, mip) = ([[0u128;101]; 101], [0u128; 101], n as usize, mip as usize);
+        dp[0][0]=1; mask[0]=1;
+        for (&g, &p) in g.iter().zip(p.iter()) {
+            for i in (0..(n as i32-g+1).max(0) as usize).rev() {
+                let x = mask[i];
+                for j in (0..=mip).filter(|&j| (1<<j)&x>0) {
+                    let jj = (j+p as usize).min(mip);
+                    mask[i+g as usize]|=1<<jj;
+                    dp[i+g as usize][jj]+=dp[i][j]
+                }
+            }
+        }
+        (dp.iter().map(|x| x[mip]).sum::<u128>() % 1000000007) as i32
     }
-}
-//profit[i], group[i]
-fn solve(min_profit:i32,profit:&Vec<i32>,group:&Vec<i32>, memo:&mut Vec<Vec<Vec<i32>>>, rem:usize,idx:usize,sum:usize)->i32{
-     if idx>= profit.len(){
-        return if sum as i32>=min_profit{1} else {0}
-    }
-    if memo[idx][rem][sum]!=-1{
-        return memo[idx][rem][sum]
-    }
-
-   
-    let mut ans = solve(min_profit,profit,group,memo,rem,idx+1,sum)%MOD;
-    if rem >=group[idx] as usize{
-        ans = (ans+solve(min_profit,profit,group,memo,rem-group[idx] as usize,idx+1,sum+profit[idx] as usize)%MOD)%MOD;
-    }
-    memo[idx][rem][sum]=ans;
-    ans
 }
