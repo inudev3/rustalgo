a30236a7ebcfb9e55228313b0b59d92f1d72cb69
Inu Jung <inust33@gmail.com>
2023-06-17 16:41:14 +0900

    Time: 55 ms (33.33%), Space: 2.2 MB (66.67%) - LeetHub

---
+++ 0993-tallest-billboard/0993-tallest-billboard.rs
@@ -0,0 +1,20 @@
+use std::collections::HashMap;
+impl Solution {
+    pub fn tallest_billboard(rods: Vec<i32>) -> i32 {
+        let mut dp = HashMap::new();
+        dp.insert(0,0);
+        for &rod in &rods{
+            let mut newdp = dp.clone();
+            for (&diff,&taller) in &dp{
+                let shorter = taller-diff;
+                let newTall = *newdp.get(&(diff+rod)).unwrap_or(&0);
+                newdp.insert(diff+rod,std::cmp::max(newTall,taller+rod));
+                let newdiff = (shorter-taller+rod).abs();
+                let newTall2 = std::cmp::max(shorter+rod,taller);
+                newdp.insert(newdiff,std::cmp::max(newTall2,*newdp.get(&newdiff).unwrap_or(&0)));
+            }
+            dp = newdp;
+        }
+        *dp.get(&0).unwrap_or(&0)
+    }
+}
