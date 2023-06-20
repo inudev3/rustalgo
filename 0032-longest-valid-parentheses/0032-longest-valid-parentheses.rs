4d696e4e03e6168e48e910d4b732fa5a2519010d
Inu Jung <inust33@gmail.com>
2023-06-20 19:56:05 +0900

    Time: 2 ms (72.97%), Space: 2.3 MB (48.65%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.rs
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.rs
@@ -3,21 +3,17 @@ impl Solution {
         let s =s.as_bytes();
         let n = s.len();
         let mut dp = vec![0;n];
-        let mut ans = 0;
+        let mut ans=0;
         for i in 1..n{
             if s[i]==b')'{
                 if s[i-1]==b'('{
-                    dp[i] = if i>=2{dp[i-2]+2} else{2};
-                }else if i-dp[i-1]>0&& s[i-dp[i-1]-1]==b'('{
-                    dp[i] = dp[i-1]+ if i-dp[i-1]>=2{
-                        dp[i-dp[i-1]-2]+2
-                    }else{
-                        2
-                    };
+                    dp[i] = if i>=2{dp[i-2]}else{0}+2;
+                }else if i as i32>dp[i-1] && s[i-dp[i-1] as usize-1]==b'('{
+                    dp[i] = dp[i-1]+if i as i32>=dp[i-1]+2{dp[i-dp[i-1] as usize-2]}else{0}+2;
                 }
-                ans = std::cmp::max(ans,dp[i]);
+                ans=std::cmp::max(ans,dp[i]);
             }
         }
-        ans as i32
+        ans
     }
 }
