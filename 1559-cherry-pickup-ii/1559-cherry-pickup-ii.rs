3ffe0db46d4561c854b707e5f1d75538efcfadd2
Inu Jung <inust33@gmail.com>
2023-06-22 16:27:09 +0900

    Time: 25 ms (75%), Space: 3.8 MB (25%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.rs
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.rs
@@ -7,25 +7,24 @@ impl Solution {
     }
 }
 fn solve(i:usize,j1:i32, j2:i32,memo:&mut Vec<Vec<Vec<i32>>>,grid:&Vec<Vec<i32>>)->i32{
-    if i>=grid.len(){
+    if i==grid.len(){
         return 0
     }
     if memo[i][j1 as usize][j2 as usize]!=-1{
-        return memo[i][j1 as usize ][j2 as usize]
+        return memo[i][j1 as usize][j2 as usize]
     }
-    let mut ans=grid[i][j1 as usize];
+    let mut ans = grid[i][j1 as usize];
     if j1!=j2{
         ans+=grid[i][j2 as usize];
     }
-    let n = memo[0].len();
+    let n = grid[0].len();
     let mut Max=0;
-    for a in std::cmp::max(0,j1-1)..=std::cmp::min(n as i32-1,j1+1){
-        for b in std::cmp::max(0,j2-1)..=std::cmp::min(n as i32-1,j2+1){
-            Max = std::cmp::max(Max, solve(i+1, a,b,memo,grid));
+    for n1 in i32::max(j1-1,0)..=i32::min(n as i32 -1,j1+1){
+        for n2 in i32::max(j2-1,0)..=i32::min(n as i32 -1,j2+1){
+            Max = i32::max(Max,  solve(i+1,n1,n2,memo,grid));
         }
     }
     ans+=Max;
-    
     memo[i][j1 as usize][j2 as usize]=ans;
     ans
 }
