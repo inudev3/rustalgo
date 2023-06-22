eb1e1b5b94c97134153c3e5e571ccf2c92e70190
Inu Jung <inust33@gmail.com>
2023-06-22 17:34:01 +0900

    Time: 12 ms (77.78%), Space: 6 MB (11.11%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.rs
+++ 0403-frog-jump/0403-frog-jump.rs
@@ -1,29 +1,29 @@
 impl Solution {
     pub fn can_cross(stones: Vec<i32>) -> bool {
         let n = stones.len();
-        let mut dp = vec![vec![-1;n];n];
-        solve(&stones, &mut dp, 0,0)==1
+        let mut dp = vec![vec![None;n];n];
+        solve(&stones, &mut dp, 0, 0)
     }
 }
-fn solve(stones:&Vec<i32>,memo:&mut Vec<Vec<i32>>, idx:usize, prevjump:usize)->i32{
-    if memo[idx][prevjump]!=-1{
-        return memo[idx][prevjump]
+fn solve(stones:&Vec<i32>, memo:&mut Vec<Vec<Option<bool>>>, idx:usize, prevjump:usize) -> bool {
+    if memo[idx][prevjump].is_some() {
+        return memo[idx][prevjump].unwrap();
     }
-    for i in -1..=1  as i32{
+    for i in -1..=1 {
         let next_jump = prevjump as i32 + i;
         if next_jump > 0 {
             let next_stone = stones[idx] + next_jump;
             match stones[(idx+1)..].binary_search(&next_stone) {
                 Ok(i) => {
-                    if solve(stones, memo, idx + i + 1, next_jump as usize) == 1 {
-                        memo[idx][prevjump] = 1;
-                        return 1
+                    if solve(stones, memo, idx + i + 1, next_jump as usize) {
+                        memo[idx][prevjump] = Some(true);
+                        return true
                     }
                 },
                 _ => continue,
             }
         }
     }
-    memo[idx][prevjump] = if idx==stones.len()-1{1}else{0};
-    return memo[idx][prevjump]
+    memo[idx][prevjump] = Some(idx == stones.len() - 1);
+    memo[idx][prevjump].unwrap()
 }
