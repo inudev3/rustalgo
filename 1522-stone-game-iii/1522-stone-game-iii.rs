d1ab86c9c49212f843ecc0633cbceb894e9be916
Inu Jung <inust33@gmail.com>
2023-06-23 17:53:51 +0900

    Time: 64 ms (31.68%), Space: 7.1 MB (24.81%) - LeetHub

---
+++ 1522-stone-game-iii/1522-stone-game-iii.rs
@@ -0,0 +1,27 @@
+impl Solution {
+    pub fn stone_game_iii(stone_value: Vec<i32>) -> String {
+        let n  = stone_value.len();
+        let mut dp = vec![i32::MIN;n];        
+        let res = solve(0, &stone_value, &mut dp);
+        let res = match res{
+            i if i>0=> "Alice",
+            i if i<0=> "Bob",
+            _=> "Tie"
+        };
+        res.to_string()
+    }
+}
+fn solve(idx:usize, stones:&Vec<i32>, memo:&mut Vec<i32>)->i32{
+    if idx>= stones.len(){        
+        return 0
+    }
+    if memo[idx]!=i32::MIN{
+        return memo[idx]
+    }
+    let mut ans = 0;
+    for j in idx..std::cmp::min(idx+3, stones.len()){
+        ans+=stones[j];
+        memo[idx]= std::cmp::max(memo[idx],ans-solve(j+1,stones,memo));
+    }
+    memo[idx]
+}
