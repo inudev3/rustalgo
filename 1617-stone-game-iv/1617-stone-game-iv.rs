dffaeb993263f446c3900aab65360a19c17c01d3
Inu Jung <inust33@gmail.com>
2023-06-23 17:21:14 +0900

    Time: 3 ms (94.74%), Space: 2.2 MB (57.89%) - LeetHub

--- 1617-stone-game-iv/1617-stone-game-iv.rs
+++ 1617-stone-game-iv/1617-stone-game-iv.rs
@@ -2,9 +2,9 @@ impl Solution {
     pub fn winner_square_game(n: i32) -> bool {
         let n = n as usize;
         let mut dp = vec![false;n +1];
-        for i in 0..=n {
+        for i in 0..=n{
             if dp[i]{
-                continue
+                continue;
             }
             for j in 1..{
                 if i+j*j>n{
