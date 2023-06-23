48a1f7d86cd2c42b60b542438ec8734784036004
Inu Jung <inust33@gmail.com>
2023-06-23 19:19:14 +0900

    Time: 39 ms (50%), Space: 12.8 MB (50%) - LeetHub

--- 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.rs
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.rs
@@ -2,20 +2,13 @@ impl Solution {
     pub fn max_value(mut events: Vec<Vec<i32>>, k: i32) -> i32 {
         let n = events.len();
         events.sort_by_key(|event|event[0]);
-        let mut dp = vec![vec![-1;n];k as usize+1];
-        solve1(k as usize,0,&events,&mut dp)
+        let mut dp = vec![vec![0;k as usize+1];n+1];
+        for i in (0..n).rev(){
+            let next_idx =events[(i+1)..].partition_point(|arr|arr[0]<=events[i][1])+i+1;
+            for j in 1..=k as usize{
+                dp[i][j] = std::cmp::max(dp[i+1][j],events[i][2]+dp[next_idx][j-1]);
+            }
+        }
+        dp[0][k as usize]
     }
-}
-fn solve1(rem:usize,idx:usize, events:&Vec<Vec<i32>>, memo:&mut Vec<Vec<i32>>)->i32{
-    if rem==0||idx==events.len(){
-        return 0
-    }
-    if memo[rem][idx]!=-1{
-        return memo[rem][idx]
-    }
-    let mut ans = solve1(rem,idx+1,events,memo);
-    let next_idx=events[(idx+1)..].partition_point(|arr|arr[0]<=events[idx][1])+idx+1;
-    ans = ans.max(solve1(rem-1,next_idx, events,memo)+events[idx][2]);
-    memo[rem][idx]=ans;
-    ans
 }
