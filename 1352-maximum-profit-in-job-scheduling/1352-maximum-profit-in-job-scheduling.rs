4d1a86fafc7d36dd4a1f69affa94279692a37091
Inu Jung <inust33@gmail.com>
2023-06-24 11:16:57 +0900

    Time: 14 ms (50%), Space: 6.6 MB (12.5%) - LeetHub

--- 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.rs
+++ 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.rs
@@ -1,16 +1,13 @@
 impl Solution {
     pub fn job_scheduling(start_time: Vec<i32>, end_time: Vec<i32>, profit: Vec<i32>) -> i32 {
-        let mut merged = vec![];
-        for i in 0..start_time.len(){
-            merged.push((start_time[i],end_time[i],profit[i]));
-        }
-        merged.sort_by_key(|tuple|tuple.0);
+        let mut merged :Vec<(i32,i32,i32)>=start_time.into_iter().zip(end_time.into_iter()).zip(profit.into_iter()).map(|((a,b),c)|(a,b,c)).collect();
+        merged.sort_unstable_by_key(|k|k.0);
         let n = merged.len();
         let mut dp = vec![-1;n];
         solve2(&merged,&mut dp, 0)
     }
 }
-fn solve2(schedule:&Vec<(i32,i32,i32)>, memo:&mut Vec<i32>, idx:usize)->i32{
+fn solve2(schedule:&[(i32,i32,i32)], memo:&mut [i32], idx:usize)->i32{
     if idx==schedule.len(){
         return 0
     }
