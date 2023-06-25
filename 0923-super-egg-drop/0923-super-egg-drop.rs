30ffdf15f14bd48541d3f8ff79585daf61989605
Inu Jung <inust33@gmail.com>
2023-06-25 10:52:27 +0900

    Time: 28 ms (50%), Space: 5.2 MB (50%) - LeetHub

--- 0923-super-egg-drop/0923-super-egg-drop.rs
+++ 0923-super-egg-drop/0923-super-egg-drop.rs
@@ -1,43 +1,42 @@
-use std::collections::HashMap;
-
-fn solve(memo: &mut HashMap<(usize, usize), i32>, floor: usize, rem: usize) -> i32 {
-    if let Some(&val) = memo.get(&(floor, rem)) {
-        return val;
-    }
-    
-    let ans = if floor == 0 {
-        0
-    } else if rem == 1 {
-        floor as i32
-    } else {
-        let mut lo = 1;
-        let mut hi = floor;
-        while lo + 1 < hi {
-            let mid = lo + (hi - lo) / 2;
-            let t1 = solve(memo, mid - 1, rem - 1);
-            let t2 = solve(memo, floor - mid, rem);
-            if t1 < t2 {
-                lo = mid;
-            } else if t1 > t2 {
-                hi = mid;
-            } else {
-                lo = mid;
-                hi=mid;
-            }
-        }
-        1 + std::cmp::min(
-            std::cmp::max(solve(memo, lo - 1, rem - 1), solve(memo, floor - lo, rem)),
-            std::cmp::max(solve(memo, hi - 1, rem - 1), solve(memo, floor - hi, rem)),
-        )
-    };
-
-    memo.insert((floor, rem), ans);
-    ans
-}
+use std::cmp::{max, min};
 
 impl Solution {
     pub fn super_egg_drop(k: i32, n: i32) -> i32 {
-        let mut memo: HashMap<(usize, usize), i32> = HashMap::new();
-        solve(&mut memo, n as usize, k as usize)
+        let mut dp = vec![vec![-1; n as usize + 1]; k as usize + 1];
+        solve(k as usize, n as usize, &mut dp)
     }
+}
+fn solve(k: usize, n: usize, dp: &mut Vec<Vec<i32>>) -> i32 {
+    if n == 0 || n == 1 {
+        return n as i32;
+    }
+    if k == 1 {
+        return n as i32;
+    }
+    if dp[k][n] != -1 {
+        return dp[k][n];
+    }
+
+    let mut ans = i32::MAX;
+    let mut l = 1;
+    let mut r = n;
+    let mut temp = 0;
+
+    while l < r {
+        let mid = l + (r - l + 1) / 2;
+        let left =solve(k - 1, mid - 1, dp);
+        let right =solve(k, n - mid, dp);
+        temp = 1 + max(left, right);
+
+        if left <= right {
+            l = mid;
+        } else {
+            r = mid - 1;
+        }
+
+        ans = min(ans, temp);
+    }
+
+    dp[k][n] = ans;
+    ans
 }
