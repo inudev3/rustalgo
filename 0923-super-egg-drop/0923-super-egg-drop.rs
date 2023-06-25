cbefb20aa0b5a0c9b8d3c1e793650671a4c177aa
Inu Jung <inust33@gmail.com>
2023-06-25 12:09:29 +0900

    Time: 16 ms (100%), Space: 5.1 MB (50%) - LeetHub

--- 0923-super-egg-drop/0923-super-egg-drop.rs
+++ 0923-super-egg-drop/0923-super-egg-drop.rs
@@ -1,47 +1,40 @@
-use std::cmp::{max, min};
-
-impl Solution {
-    pub fn super_egg_drop(k: i32, n: i32) -> i32 {
-        let mut dp = vec![vec![-1; n as usize + 1]; k as usize + 1];
-        solve(k as usize, n as usize, &mut dp)
-    }
-}
-fn solve(k: usize, n: usize, dp: &mut Vec<Vec<i32>>) -> i32 {
+use std::collections::HashMap;
+use std::cmp::{min,max};
+fn solve(memo: &mut Vec<Vec<i32>>, n: usize, k: usize) -> i32 {
     if n == 0 || n == 1 {
         return n as i32;
     }
     if k == 1 {
         return n as i32;
     }
-    if dp[k][n] != -1 {
-        return dp[k][n];
+    if memo[k][n] != -1 {
+        return memo[k][n];
     }
-
-    let mut ans = i32::MAX;
-    let mut l = 1;
-    let mut r = n;
-    let mut temp = 0;
-
-    while l+1 < r {
-        let mid = l + (r - l + 1) / 2;
-        let left =solve(k - 1, mid - 1, dp);
-        let right =solve(k, n - mid, dp);
-
-
-        if left < right {
-            l = mid;
-        } else if left>right{
-            r = mid ;
-        }else{
-            dp[k][n] = 1+left;
-            return 1+left
+    let mut lo = 1;
+    let mut hi = n;
+    while lo + 1 < hi {
+        let mid = lo + (hi - lo) / 2;
+        let t1 = solve(memo, mid - 1, k - 1);
+        let t2 = solve(memo, n - mid, k);
+        if t1 < t2 {
+            lo = mid;
+        } else if t1 > t2 {
+            hi = mid;
+        } else {
+            lo = mid;
+            hi=mid;
         }
     }
-    ans = 1+ min(
-        max(solve(k-1,l-1,dp),solve(k,n-l,dp)),
-        max(solve(k-1,r-1,dp),solve(k,n-r,dp))
+    memo[k][n]=1 + min(
+        max(solve(memo, lo - 1, k - 1), solve(memo, n - lo, k)),
+        max(solve(memo, hi - 1, k - 1), solve(memo, n - hi, k)),
     );
+    memo[k][n]
+}
 
-    dp[k][n] = ans;
-    ans
+impl Solution {
+    pub fn super_egg_drop(k: i32, n: i32) -> i32 {
+        let mut memo = vec![vec![-1;n as usize+1];k as usize+1];
+        solve(&mut memo, n as usize, k as usize)
+    }
 }
